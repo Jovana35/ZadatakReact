1151d317f6b4d374d00bff6f62566dff5f6b6c1c
Jovana Miladinovic <mjovanaam@gmail.com>
2022-01-02 20:24:44 +0100

    Kreirana je ReactJS aplikacija.

--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+//container je mesto gde ce element da se renderuje
+//umetni element na lokaciju containera
 ReactDOM.render(
+  //element
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  //container
+  //pretrazi dokument po id-ju i nadji onaj ciji je id root
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
