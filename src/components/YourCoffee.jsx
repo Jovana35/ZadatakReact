26d1b141082dce260816b9f593f4b269e082f7ce
Jovana Miladinovic <mjovanaam@gmail.com>
2022-01-06 18:35:00 +0100

    Ispravljene greške u fajlu YourCoffee.jsx.

--- src/components/YourCoffee.jsx
+++ src/components/YourCoffee.jsx
@@ -2,7 +2,7 @@ import OneCoffee from "./OneCoffee";
 const YourCoffee = ({coffee}) => {
     return (
         <div className="cart-container">
-            <h3>This is your coffee.</h3>
+            <h3>Your order is ready. ♥</h3>
             {coffee.map((cof) => (
                  <OneCoffee oneCoffee={cof} key={cof.id} ordered={0}/>
              ))}
